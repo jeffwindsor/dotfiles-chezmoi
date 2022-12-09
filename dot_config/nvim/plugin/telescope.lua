c98d455005779c1a2c59633d6cb90bffd8e183be
Windsor, Jeff <jwindsor@cj.com>
2022-12-08 20:39:31 -0800

    Update .config/nvim/plugin/telescope.lua

--- dot_config/nvim/plugin/telescope.lua
+++ dot_config/nvim/plugin/telescope.lua
@@ -4,18 +4,10 @@ local opts = { noremap = true, silent = true }
 local telescope = require('telescope')
 
 telescope.setup{
-    defaults = {
-      layout_strategy = 'vertical',
-      layout_config = { prompt_position = 'top' },
-      sorting_strategy = 'ascending',
-    },
-    extensions = {
-      file_browser = {
-        hidden = true,
-        --layout_strategy = 'horizontal',
-      }
-    }
-  }
+  defaults = {
+    layout_strategy = 'vertical',
+    layout_config = { prompt_position = 'top' },
+    sorting_strategy = 'ascending',
+  },
+}
 
-telescope.load_extension('file_browser')
---telescope.load_extension('project')
