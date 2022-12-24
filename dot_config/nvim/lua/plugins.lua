a63d49a313bbbb4545f3131544d086dd40152036
Jeff Windsor <jeff.windsor@gmail.com>
2022-12-24 06:19:44 -0800

    Remove .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-require 'plugins.packer'
-
-require 'plugins.auto-save'
-require 'plugins.indent-blankline'
-require 'plugins.lualine'
-require 'plugins.material-nvim'
-require 'plugins.nvim-treesitter'
-require 'plugins.telescope'
-require 'plugins.which-key'
