e2feaea523c3eaa56018f4b79b76b6799caa4d93
Jeff Windsor <jeff.windsor@gmail.com>
2022-12-24 09:35:49 -0800

    Add .config/nvim/after/plugin/fidget.lua Add .config/nvim/after/plugin/mason.lua Add .config/nvim/after/plugin/neodev.lua Add .config/nvim/after/plugin/nvim-cmp.lua Update .config/nvim/after/plugin/nvim-treesitter.lua

---
+++ dot_config/nvim/after/plugin/mason.lua
@@ -0,0 +1,38 @@
+require('mason').setup()
+
+-- Enable the following language servers
+local servers = {
+  --clangd = {},
+  --gopls = {},
+  pyright = {},
+  rust_analyzer = {},
+  --tsserver = {},
+
+  sumneko_lua = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+}
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    require('lspconfig')[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+    }
+  end,
+}
