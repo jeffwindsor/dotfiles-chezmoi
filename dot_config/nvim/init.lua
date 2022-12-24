bb549498d7d280cb876eb9e9858e1118812880d6
Jeff Windsor <jeff.windsor@gmail.com>
2022-12-24 07:45:22 -0800

    Update .config/nvim/init.lua Update .config/nvim/lua/plugins/packer.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,33 +1,49 @@
 -- ============================================================================
+-- PLUGINS : explicit loading. 
+--  for auto loading, move source files to .config/nvim/after/plugin/*.lua 
+--  see :help load-plugins for more info
+-- ============================================================================
+require 'plugins.packer' -- done first by design
+
+require 'plugins.auto-save'
+require 'plugins.indent-blankline'
+require 'plugins.lualine'
+require 'plugins.material-nvim'
+require 'plugins.nvim-treesitter'
+require 'plugins.telescope'
+require 'plugins.which-key'
+
+-- ============================================================================
 -- OPTIONS
 -- ============================================================================
-vim.opt.autoindent	    = true
-vim.opt.background	    = 'dark'
+vim.opt.autoindent      = true
+vim.opt.background      = 'dark'
 vim.opt.backspace       = 'indent,eol,start'
 vim.opt.clipboard       = 'unnamedplus' 
-vim.opt.cursorline	    = true            
+vim.opt.cursorline      = true            
 vim.opt.encoding        = 'UTF-8'
-vim.opt.expandtab       = true			    -- spaces over tabs
-vim.opt.hidden          = true			    -- allow background buffers 
-vim.opt.hlsearch        = true			    -- highlight found searches
-vim.opt.ignorecase	    = true        
-vim.opt.inccommand	    = 'split'		    -- get preview of replacements
-vim.opt.incsearch 	    = true       		-- show match while typing
+vim.opt.expandtab       = true          -- spaces over tabs
+vim.opt.hidden          = true          -- allow background buffers 
+vim.opt.hlsearch        = true          -- highlight found searches
+vim.opt.ignorecase      = true        
+vim.opt.inccommand      = 'split'        -- get preview of replacements
+vim.opt.incsearch       = true           -- show match while typing
 vim.opt.mouse           = 'a'           -- turn on mouse usage
 vim.opt.number          = true
-vim.opt.relativenumber  = true			    -- hybrid number scheme
+vim.opt.relativenumber  = true          -- hybrid number scheme
 vim.opt.ruler           = true
-vim.opt.shiftwidth	    = 2
+vim.opt.shiftwidth      = 2
 vim.opt.smartindent     = true
 vim.opt.smarttab        = true
-vim.opt.softtabstop	    = 2
+vim.opt.softtabstop     = 2
 vim.opt.spell           = false
 vim.opt.spelllang       = 'en_us'
-vim.opt.splitbelow	    = true
-vim.opt.splitright	    = true
-vim.opt.tabstop		      = 2
-vim.opt.termguicolors  	= true
-vim.opt.wrap	          = false
+vim.opt.splitbelow      = true
+vim.opt.splitright      = true
+vim.opt.tabstop         = 2
+vim.opt.termguicolors   = true
+vim.opt.wrap            = false
+
 
 -- ============================================================================
 -- AUTOCOMMANDS
@@ -44,11 +60,8 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", 
 -- ============================================================================
 -- KEY MAPPINGS: note <leader> key maps moved to WHICH KEY plugin lua file
 -- ============================================================================
--- protects from remapping by other configs
-local noremap = { noremap = true }
-
--- silent does not show bound command in bottom row / command output
-local silent  = { noremap = true, silent = true }
+local noremap = { noremap = true }                -- protects from remapping by other configs
+local silent  = { noremap = true, silent = true } -- silent does not show bound command in bottom row / command output
 
 -- PERSONAL home row improvements
 --  quick <jk> produces an <escape> while in insert mode
@@ -80,18 +93,5 @@ vim.api.nvim_set_keymap('v', ';', ':', noremap)
 vim.api.nvim_set_keymap('n', 'Y', 'y$', noremap)
 
 
--- ============================================================================
--- PLUG IN CONFIGURATION
--- ============================================================================
-require 'plugins.packer'
-
-require 'plugins.auto-save'
-require 'plugins.indent-blankline'
-require 'plugins.lualine'
-require 'plugins.material-nvim'
-require 'plugins.nvim-treesitter'
-require 'plugins.telescope'
-require 'plugins.which-key'
 
 vim.cmd 'colorscheme kanagawa'
-
