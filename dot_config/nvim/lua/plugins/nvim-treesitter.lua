dfe820f1eec49468a1abc852a283655942d60089
Jeff Windsor <jeff.windsor@gmail.com>
2022-12-24 07:47:45 -0800

    Remove .config/nvim/lua/plugins/auto-save.lua Remove .config/nvim/lua/plugins/indent-blankline.lua Remove .config/nvim/lua/plugins/lualine.lua Remove .config/nvim/lua/plugins/material-nvim.lua Remove .config/nvim/lua/plugins/nvim-treesitter.lua Remove .config/nvim/lua/plugins/packer.lua Remove .config/nvim/lua/plugins/telescope.lua Remove .config/nvim/lua/plugins/which-key.lua

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++
@@ -1,8 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c', 'lua', 'rust' },
-  sync_install = false,
-  auto_install = true,
-  highlight = { 
-    enable = true, 
-  },
-}
