70b0b34a45f9e9d815f316318db1ce2511f8c17d
Windsor, Jeff <jwindsor@cj.com>
2023-02-16 15:04:23 -0800

    Add .config/installs/base.nix

---
+++ dot_config/installs/base.nix
@@ -0,0 +1,21 @@
+with import <nixpkgs> {}; [
+  alacritty
+  bat
+  bottom
+  #chrome
+  exa
+  fd
+  fish
+  fzf
+  jetbrains-mono
+  #jetbrains-toolbox
+  #jetbrains-intellij
+  helix
+  neovim
+  megacmd
+  ripgrep
+  starship
+  tealdeer
+  topgrade
+  zellij
+]
