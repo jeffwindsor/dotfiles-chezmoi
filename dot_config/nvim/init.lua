7e2707522c416b9b75c0158845d54d85d10cd40f
Windsor, Jeff <jwindsor@cj.com>
2023-03-02 18:26:30 -0800

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,20 +1,29 @@
 -- ----------------------------------------------------------------------------
--- PLUGINS
+-- PLUGIN MANAGER
 -- ----------------------------------------------------------------------------
-require("bootstrap.lazy")
+-- bootstrap lazy nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- load plugins
 require("lazy").setup({
-  -- ----------------------------------------------------------------------------------------------
-  -- personal workflow
-  -- ----------------------------------------------------------------------------------------------
   'pocco81/auto-save.nvim',                  -- auto save on return to normal mode
   {'echasnovski/mini.nvim', version = '*', lazy = true},
   'nvim-lua/plenary.nvim',
   'nvim-telescope/telescope.nvim',           -- Fuzzy Finder (files, lsp, etc)
   {'folke/which-key.nvim', lazy = true},     -- display key bindings (like emacs)
 
-  -- ----------------------------------------------------------------------------------------------
   -- color schemes
-  -- ----------------------------------------------------------------------------------------------
   {'folke/tokyonight.nvim', lazy = true},
   {'joshdick/onedark.vim', lazy = true},
   {'marko-cerovac/material.nvim', lazy = true},
@@ -24,11 +33,19 @@ require("lazy").setup({
 
 })
 
+-- ----------------------------------------------------------------------------
+-- COLORSCHEME DEFAULT
+-- ----------------------------------------------------------------------------
+vim.cmd 'colorscheme tokyonight-storm'
 
--- plugin setups
+-- ----------------------------------------------------------------------------
+-- PLUGIN: AUTO-SAVE
+-- ----------------------------------------------------------------------------
 require("auto-save").setup({ enabled = true })
---require('plugin.lualine')
 
+-- ----------------------------------------------------------------------------
+-- PLUGIN: MINI
+-- ----------------------------------------------------------------------------
 require('mini.ai').setup()
 require('mini.align').setup()
 -- require('mini.base16').setup()	      -- supply palette instead of color schemes?
@@ -44,6 +61,9 @@ require('mini.surround').setup()        -- surrond object with ({', etc
 require('mini.tabline').setup()
 require('mini.trailspace').setup()      -- highlights trailing whitespace
 
+-- ----------------------------------------------------------------------------
+-- PLUGIN: TELESCOPE
+-- ----------------------------------------------------------------------------
 require('telescope').setup{
   defaults = {
     layout_strategy = 'vertical',
@@ -51,12 +71,131 @@ require('telescope').setup{
     sorting_strategy = 'ascending',
   },
 }
-require('plugin.which-key')
-
 -- ----------------------------------------------------------------------------
--- COLORSCHEME
+-- PLUGIN: WHICH KEY
 -- ----------------------------------------------------------------------------
-vim.cmd 'colorscheme tokyonight-storm'
+local buffer_next = ':bnext<CR>'
+local load_config = ':luafile %<CR>'
+local wk = require('which-key')
+wk.setup()
+wk.register({
+  ["<leader>"] = { ':Telescope spell_suggest<CR>', 'spelling'},
+  ["."] = { load_config .. ':echo "Configuration Re-Loaded"<CR>', 'load config'},
+  [","] = {
+    name = 'commands',
+    [","] = { ':Telescope commands<CR>','search commands'},
+    a = { ':Telescope autocommands<CR>', 'search auto commands' },
+    h = { ':Telescope command_history<CR>','search command history'},
+    p = { ':Telescope builtin<CR>','pickers'},
+  },
+  b = {
+    name = 'buffers',
+    b = { ':Telescope buffers<CR>','buffers'},
+    c = { ':bdelete<CR>','close'},
+    j = { ':bprev<CR>','previous'},
+    k = { buffer_next,'next'},
+    n = { ':enew<CR>','new'},
+    s = { ':Telescope current_buffer_fuzzy_find<CR>', 'search text'},
+    t = { ':Telescope current_buffer_tags<CR>', 'search tags' },
+  },
+  c = {
+    name = 'color schemes',
+    c = { ':Telescope colorscheme<CR>','color schemes'},
+    d = { ':set background=dark<CR>','dark'},
+    l = { ':set background=light<CR>','light'},
+    m = { ':lua require("material.functions").find_style()<CR>', 'material' },
+    o = { ':colorscheme OceanNext<CR>', 'ocean next' },
+    k = { ':colorscheme kanagawa<CR>', 'kanagawa' },
+    n = { ':colorscheme nord<CR>', 'nord' },
+    t = {
+      name = 'tokyo night',
+      n = { ':colorscheme tokyonight-night<CR>', 'night' },
+      s = { ':colorscheme tokyonight-storm<CR>', 'storm' },
+      d = { ':colorscheme tokyonight-day<CR>', 'day' },
+      m = { ':colorscheme tokyonight-moon<CR>', 'moon' },
+    },
+    ["1"] = { ':colorscheme onedark<CR>', 'one dark' },
+  },
+  f = {
+    name = 'files',
+    b = { ':Telescope file_browser<CR>','via browser'},
+    c = { ':Telescope find_files cwd=$XDG_CONFIG_HOME<CR>','in config directory'},
+    d = { ':Telescope find_files cwd=$DOTFILES<CR>','in dotfiles directory'},
+    f = { ':Telescope find_files<CR>','in current directory'},
+    g = { ':Telescope find_files cwd=$SRC<CR>','in git repos'},
+    h = { ':Telescope find_files cwd=$HOME<CR>','in home directory'},
+    i = { ':Telescope find_files cwd=$INSTALLS<CR>','in install files directory'},
+    p = { ':Telescope project_files<CR>','in project'},
+    s = { ':Telescope live_grep<CR>','grep'},
+    t = { ':Telescope filetypes<CR>','set file type'},
+  },
+  g = {
+    name = 'git',
+    b = { ':Telescope git_branches<CR>','branches'},
+    c = { ':Telescope git_commits<CR>','commits'},
+    f = { ':Telescope git_files<CR>','files'},
+    s = { ':Telescope git_status<CR>','status'},
+  },
+  h = {
+    name = 'help',
+    h = { ':Telescope help_tags<CR>','topics'},
+    m = { ':Telescope help_tags<CR>','man-pages'},
+  },
+  j = { ':Telescope jumplist<CR>', 'jump lists'},
+  k = { ':Telescope keymaps<CR>', 'key maps'},
+  l = {
+    name = 'lsp',
+    d = { ':Telescope lsp_definitions<CR>','definition'},
+    i = { ':Telescope lsp_implementations<CR>','implementations'},
+    r = { ':Telescope lsp_references<CR>','references'},
+    t = { ':Telescope lsp_type_definitions<CR>','type'},
+  },
+  m = {
+    name = 'mini nvim',
+    a = {
+      name = 'align <TBD>',
+    },
+    c = {
+      name = 'comment',
+      l = {'gcc','toggle line (gcc)'},
+    },
+    t = {
+      name = 'trim whitespace',
+      l = { ':lua MiniTrailspace.trim_last_lines()', 'trailing' },
+      w = { ':lua MiniTrailspace.trim()', 'last lines' },
+    },
+  },
+  p = { ':Lazy<CR>', 'package manager'},
+  q = { ':q<CR>','quit'},
+  r = { ':Telescope registers<CR>','registers'},
+  t = {
+    name = 'tabs',
+    c = { ':tabclose<CR>','close'},
+    j = { ':tabprevious<CR>','previous'},
+    k = { ':tabnext<CR>','next'},
+    n = { ':tabnew<CR>','new'},
+  },
+  w = {
+    name = 'windows',
+    c = { ':close<CR>','close'},
+    h = {'<C-W><left>','left'},
+    j = {'<C-W><down>','down'},
+    k = {'<C-W><up>','up'},
+    l = {'<C-W><right>','right'},
+    s = { ':split<CR>' .. buffer_next,'split down'},
+    v = { ':vsplit<CR>' .. buffer_next,'split right'},
+    x = {
+      name = 'resize',
+      h = { ':vertical resize -5<CR>','left'},
+      j = { ':resize -5<CR>','down'},
+      k = { ':resize +5<CR>','up'},
+      l = { ':vertical resize +5<CR>','right'},
+    },
+  },
+  x = { ':q!<CR>', 'force quit'},
+},
+{ prefix = '<leader>'}
+);
 
 -- ----------------------------------------------------------------------------
 -- OPTIONS: more info @ `:h vim.o`
