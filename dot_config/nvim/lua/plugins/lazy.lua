3a0a1cc26bc7e8da9eefa3064158de6d2769e248
Windsor, Jeff <jwindsor@cj.com>
2023-03-10 07:19:11 -0800

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lazy.lua

--- dot_config/nvim/lua/plugins/lazy.lua
+++ dot_config/nvim/lua/plugins/lazy.lua
@@ -18,31 +18,56 @@ vim.opt.rtp:prepend(lazypath)
 -- load plugins
 -- ----------------------------------------------------------------------------
 require("lazy").setup({
-{ 'pocco81/auto-save.nvim' },
-{ 'echasnovski/mini.nvim', version = '*', lazy = true },
-{ 'lukas-reineke/indent-blankline.nvim' },
-{ 'nvim-telescope/telescope.nvim', dependencies = {'nvim-lua/plenary.nvim'} },
-{ 'folke/which-key.nvim', lazy = true },
--- color schemes
-{'folke/tokyonight.nvim', lazy = true},
-{'joshdick/onedark.vim', lazy = true},
-{'marko-cerovac/material.nvim', lazy = true},
-{'mhartington/oceanic-next', lazy = true},
-{'rebelot/kanagawa.nvim', lazy = true},
-{'shaunsingh/nord.nvim', lazy = true},
+  -- personal workflow plugins
+  { 'pocco81/auto-save.nvim' },
+  { 'echasnovski/mini.nvim', version = '*', lazy = true },
+  { 'lukas-reineke/indent-blankline.nvim' },
+  { 'nvim-telescope/telescope.nvim', dependencies = {'nvim-lua/plenary.nvim'} },
+  { 'folke/which-key.nvim', lazy = true },
 
+  -- color schemes
+  { 'folke/tokyonight.nvim', lazy = true},
+  { 'joshdick/onedark.vim', lazy = true},
+  { 'marko-cerovac/material.nvim', lazy = true},
+  { 'mhartington/oceanic-next', lazy = true},
+  { 'rebelot/kanagawa.nvim', lazy = true},
+  { 'shaunsingh/nord.nvim', lazy = true},
+
+  -- development
+  { "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  },
+  { 'neovim/nvim-lspconfig',              -- LSP Configuration & Plugins
+    dependencies = {
+      'williamboman/mason.nvim',              -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason-lspconfig.nvim',
+      'j-hui/fidget.nvim',                    -- Useful status updates for LSP
+      'folke/neodev.nvim',                    -- Additional lua configuration, makes nvim stuff amazing
+    }},
+  { 'hrsh7th/nvim-cmp',                   -- Autocompletion
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip'
+    }},
+  { 'nvim-treesitter/nvim-treesitter' },    -- Highlight, edit, and navigate code
 })
 
 -- ----------------------------------------------------------------------------
 -- setup plugins
 -- ----------------------------------------------------------------------------
-require('plugins.which-key') 
+require('plugins.which-key')
 require('mini.ai').setup()
 require('mini.align').setup()
 -- require('mini.base16').setup()	      -- supply palette instead of color schemes?
 -- require('mini.basics').setup()     	-- leran from these settings or just use?
 require('mini.comment').setup()
-require('mini.completion').setup()
+--require('mini.completion').setup()
 require('mini.cursorword').setup()      -- highlight word under cursor matches
 --require('mini.indentscope').setup()
 require('mini.jump2d').setup()          -- leap like jumps / finds
@@ -52,8 +77,8 @@ require('mini.surround').setup()        -- surrond object with ({', etc
 require('mini.tabline').setup()
 require('mini.trailspace').setup()      -- highlights trailing whitespace
 
-require("auto-save").setup({ 
-  enabled = true 
+require("auto-save").setup({
+  enabled = true
 })
 
 require("indent_blankline").setup({
