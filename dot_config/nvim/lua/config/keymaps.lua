4dca9a36a43637f2ec374aeb8d37470d977e6cde
Jeff Windsor <jeff.windsor@gmail.com>
2023-05-07 09:22:33 -0700

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,19 +7,24 @@ local silent  = { noremap = true, silent = true } -- silent does not show bound 
 
 -- PERSONAL home row improvements
 
+vim.api.nvim_set_keymap('n', 'j', 'h', silent)
+vim.api.nvim_set_keymap('n', 'k', 'j', silent)
+vim.api.nvim_set_keymap('n', 'l', 'k', silent)
+vim.api.nvim_set_keymap('n', ';', 'l', silent)
+
 --paste over word
 vim.api.nvim_set_keymap('n', '<C-p>', 'cw<C-r>0<ESC>', silent)
 
 --  quick <jk> produces an <escape> while in insert mode
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', silent)
+-- vim.api.nvim_set_keymap('i', 'jk', '<ESC>', silent)
 
 --  buffer movement direct from CONTROL home row keys
 --    left / right for tabs
-vim.api.nvim_set_keymap("n", "<C-h>", "tabprev", silent)
-vim.api.nvim_set_keymap("n", "<C-l>", "tabnext", silent)
+vim.api.nvim_set_keymap("n", "<C-j>", "tabprev", silent)
+vim.api.nvim_set_keymap("n", "<C-;>", "tabnext", silent)
 --    up / down for buffers
-vim.api.nvim_set_keymap("n", "<C-j>", "bprev", silent)
-vim.api.nvim_set_keymap("n", "<C-k>", "bnext", silent)
+vim.api.nvim_set_keymap("n", "<C-k>", "bprev", silent)
+vim.api.nvim_set_keymap("n", "<C-l>", "bnext", silent)
 
 --  first character in line
 vim.api.nvim_set_keymap("n", "gh", "^", silent)
@@ -32,8 +37,8 @@ vim.api.nvim_set_keymap("n", "<CR>", ":noh<CR><CR>", silent)
 
 -- PERSONAL muscle memory improvements
 --  command mode without the shift key (normal and visual mode)
-vim.api.nvim_set_keymap('n', ';', ':', noremap)
-vim.api.nvim_set_keymap('v', ';', ':', noremap)
+-- vim.api.nvim_set_keymap('n', '\'', ':', noremap)
+-- vim.api.nvim_set_keymap('v', '\'', ':', noremap)
 
 --  yank acts like other capitol letters (normal mode)
 vim.api.nvim_set_keymap('n', 'Y', 'y$', noremap)
