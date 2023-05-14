dfc9969c87c29945dd68f59abfc19de987792c73
Jeff Windsor <jeff.windsor@gmail.com>
2023-05-14 15:20:14 -0700

    Update .config/fortune/build.sh Update .config/fortune/quotes

--- dot_config/fortune/executable_build.sh
+++ dot_config/fortune/executable_build.sh
@@ -4,7 +4,7 @@
 sudo rm -f /usr/share/games/fortune/*
 
 # get all files with no extension in current directory
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit
 shopt -s extglob
 files=(!(*.*))
 
@@ -12,12 +12,12 @@ files=(!(*.*))
 for file in "${files[@]}"
 do
   echo "compiling $file"
-  rm -f ${file}.dat
-  strfile -c % ${file} ${file}.dat
+  rm -f "$file".dat
+  strfile -c % "$file" "$file".dat
 
   echo "copying $file and $file.dat"
-  sudo cp ${file} /usr/share/games/fortune/
-  sudo mv ${file}.dat /usr/share/games/fortune/
+  sudo cp "$file" /usr/share/games/fortune/
+  sudo mv "$file".dat /usr/share/games/fortune/
 
   echo
 done
