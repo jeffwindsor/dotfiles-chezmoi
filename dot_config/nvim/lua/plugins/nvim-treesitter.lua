a2e22940a2cd6670f20cb49b2fe96b49bed4a717
Jeff Windsor <jeff.windsor@gmail.com>
2023-05-29 17:49:15 -0700

    Update .config/nvim/lua/plugins/nvim-treesitter.lua

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ pcall(require('nvim-treesitter.install').update { with_sync = true })
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
+  ensure_installed = { 'bash', 'c', 'go', 'lua', 'python', 'rust', 'sql', 'tsx', 'typescript', 'vim', 'yaml' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
