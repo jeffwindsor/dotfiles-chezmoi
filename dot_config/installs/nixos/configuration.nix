89ab7407eb6d3f68bab0afbebd64463522932d31
Windsor, Jeff <jwindsor@cj.com>
2023-06-16 12:30:37 -0700

    Update .config/installs/nixos/configuration.nix

--- dot_config/installs/nixos/configuration.nix
+++ dot_config/installs/nixos/configuration.nix
@@ -1,35 +1,76 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "America/Los_Angeles";
+  networking.hostName = "frame"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mid = {
+    isNormalUser = true;
+    description = "The Middle Way";
+    extraGroups = [ "networkmanager" "wheel" ];
+    # packages = with pkgs; [];
+  };
+
+  environment.systemPackages = with pkgs; [
+    alacritty
+    avahi
+    bat
+    bottom
+    broot
+    chezmoi
+    clamav
+    direnv
+    exa
+    fclones
+    fd
+    fortune
+    fzf
+    gcc
+    git
+    gnome-extension-manager
+    gnome-firmware
+    gnomeExtensions.appindicator
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.caffeine
+    gnomeExtensions.executor
+    gnomeExtensions.forge
+    gnomeExtensions.just-perfection
+    gnomeExtensions.openweather
+    gnomeExtensions.space-bar
+    gnomeExtensions.wallpaper-switcher
+    lf
+    neovim
+    ripgrep
+    sd
+    shellcheck
+    starship
+    tealdeer
+    tlp
+    zellij
+    zsh
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+
+    #google-chrome
+    chromium
+    clapper
+    firefox
+    gnucash
+    jetbrains-mono
+    librewolf
+    megasync
+    newsflash
+    obsidian
+    spotify
+  ];
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -42,114 +83,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.browsing = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.mid = {
-    isNormalUser = true;
-    description = "The Middle Way";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    ];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-	alacritty
-	bat
-	bottom
-	broot
-	clamav
-	direnv
-	exa
-	fclones
-	fd
-	fortune
-	fzf
-	gcc
-	git
-	gnome-extension-manager
-	gnome-firmware
-	lf
-	neovim
-	ripgrep
-	sd
-	shellcheck
-	starship
-	tealdeer
-	tlp
-	zellij
-
-	avahi
-	zsh
-	zsh-autosuggestions
-	zsh-syntax-highlighting
-
-
-	gnomeExtensions.appindicator
-	gnomeExtensions.blur-my-shell
-	gnomeExtensions.caffeine
-	gnomeExtensions.executor
-	gnomeExtensions.forge
-	gnomeExtensions.just-perfection
-	gnomeExtensions.openweather
-	gnomeExtensions.space-bar
-	gnomeExtensions.wallpaper-switcher
-
-	chezmoi
-	clapper
-	gnucash
-	jetbrains-mono
-	megasync
-	newsflash
-	obsidian
-	spotify
-
-	chromium
-	#google-chrome
-	librewolf
-	firefox
-  ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -157,27 +90,51 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+
+  # boot loader
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  }
+
+  # shells
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
 
-  # List services that you want to enable:
-  services.avahi.enable = true;
-  services.avahi.publish.enable = true;
-  services.avahi.publish.userServices = true;
+  # sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # services
+  services.avahi = {
+    enable = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    layout = "us";
+    xkbVariant = "";
+  };
 
+  services.printing = {
+    enable = true;
+    browsing = true;
+  };
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  system.stateVersion = "23.05";
 }
