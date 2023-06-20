4b6bf891569da3da2c57e791cd112335cfdf2b10
Jeff Windsor <jeff.windsor@gmail.com>
2023-06-19 17:27:33 -0700

    Update .config/installs/nixos/configuration.nix

--- dot_config/installs/nixos/configuration.nix
+++ dot_config/installs/nixos/configuration.nix
@@ -4,9 +4,7 @@
   imports = [ ./hardware-configuration.nix ];
 
   time.timeZone = "America/Los_Angeles";
-  networking.hostName = "frame"; # Define your hostname.
-  networking.networkmanager.enable = true;
-  nixpkgs.config.allowUnfree = true;
+  networking.hostName = "frame";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mid = {
@@ -16,6 +14,8 @@
     # packages = with pkgs; [];
   };
 
+  # system packages
+  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     alacritty
     avahi
@@ -69,35 +69,14 @@
     spotify
   ];
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # boot loader
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
+  networking.networkmanager.enable = true;
+  security.rtkit.enable = true;
 
-  # shells
+  # zsh
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
@@ -105,38 +84,57 @@
   # sound
   sound.enable = true;
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
 
-  # services
-  services.avahi = {
-    enable = true;
-    publish = {
+  services = {
+    avahi = {
       enable = true;
-      userServices = true;
+      publish = {
+        enable = true;
+        userServices = true;
+      };
+    };
+
+    printing = {
+      enable = true;
+      browsing = true;
+      nssmdns = true;
+      openFirewall = true;
+    };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      layout = "us";
+      xkbVariant = "";
     };
   };
 
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-    layout = "us";
-    xkbVariant = "";
+  # Select internationalisation properties.
+  i18n ={
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 
-  services.printing = {
-    enable = true;
-    browsing = true;
-    nssmdns = true;
-    openFirewall = true;
+  system ={
+    autoUpgrade.enable = true;
+    stateVersion = "23.05";
   };
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  system.stateVersion = "23.05";
 }
