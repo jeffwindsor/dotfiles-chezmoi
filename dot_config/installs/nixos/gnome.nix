6c4b3edcc77c15f96673b6ff5fb03d812edd486d
Jeff Windsor <jeff.windsor@gmail.com>
2023-07-01 21:46:25 -0700

    Update .config/installs/nixos/gnome.nix

--- dot_config/installs/nixos/gnome.nix
+++ dot_config/installs/nixos/gnome.nix
@@ -20,11 +20,10 @@
   };
 
   services.xserver = {
-      enable = true;
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-      layout = "us";
-      xkbVariant = "";
-    };
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    layout = "us";
+    xkbVariant = "";
   };
 }
